08d3dae16307732b16b4f37ecb2121442b3b2830
Parin <pgouraram@wisc.edu>
2024-02-29 05:17:38 +0000

    added class files and Makefile

--- ISCPlaceholder.java
+++ ISCPlaceholder.java
@@ -1,17 +1,18 @@
 import java.util.Iterator;
+import java.util.ArrayList;
 
-public class ISCPlaceholder<T extends Comparable<T>>
-    implements IterableSortedCollection<T> {
+public class ISCPlaceholder
+    implements IterableSortedCollection<SongInterface> {
 
-    private T value;
+    private SongInterface value;
     
-    public boolean insert(T data)
+    public boolean insert(SongInterface data)
 	throws NullPointerException, IllegalArgumentException {
 	value = data;
 	return true;
     }
 
-    public boolean contains(Comparable<T> data) {
+    public boolean contains(Comparable<SongInterface> data) {
 	return true;
     }
 
@@ -20,17 +21,25 @@ public class ISCPlaceholder<T extends Comparable<T>>
     }
     
     public int size() {
-	return 3;
+	return 603;
     }
 
     public void clear() {
     }
 
-    public void setIterationStartPoint(Comparable<T> startPoint) {	
+    public void setIterationStartPoint(Comparable<SongInterface> startPoint) {	
     }
 
-    public Iterator<T> iterator() {
+    public Iterator<SongInterface> iterator() {
 	
-	return java.util.Arrays.asList(value, value, value).iterator();
+	ArrayList<SongInterface> list = new ArrayList<>();
+	SongInterface song1 = new Song("Anaconda,Nicki Minaj,dance pop,2014,130,60,96,-6,21");
+	SongInterface song2 = new Song("Bad Liar,Selena Gomez,dance pop,2018,121,41,97,-6,8");
+	SongInterface song3 = new Song("Drip (feat. Migos),Cardi B,pop,2018,130,59,97,-8,8");
+
+	list.add(song1);
+	list.add(song2);
+	list.add(song3);
+	return list.iterator();
     }
 }
---
+++ Makefile
@@ -0,0 +1,10 @@
+runBDTests: 
+	javac -cp .:../junit5.jar Backend.java
+	javac -cp .:../junit5.jar Song.java
+	javac -cp .:../junit5.jar BackendDeveloperTests.java
+	javac -cp .:../junit5.jar ISCPlaceholder.java	
+	java -jar ../junit5.jar -cp . -c BackendDeveloperTests
+
+
+clean: 
+	rm -rf *.class
